50a3952a99b7e12f7d749101e572f613031d3892
Guillermo Illera Vinatea <somnoliento@duck.com>
2026-06-27 00:41:20 +0200

    Añadidas mejoras milescelaneas

--- service-worker.js
+++ service-worker.js
@@ -62,6 +62,11 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.pathname.endsWith('robots.txt') || url.pathname.endsWith('sitemap.xml')) {
+    e.respondWith(fetch(e.request));
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then((cached) => {
       const fetched = fetch(e.request).then((res) => {
